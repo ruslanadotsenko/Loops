5bbe3dc6c04cb8c3ae2eb63909543a51c5f0d990
Ruslana Kolomatska <ruslana.dotsenko@gmail.com>
2023-09-03 22:58:35 +0300

    'for' loop was replaced with enhanced 'for'

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,9 +7,9 @@ public class Main {
         int sum = 0;
         int numeration = 1;
 
-        for (int i = 0; i < numArr.length; i++) {
-            sum += numArr[i];
-            System.out.println(numeration + ") Num is " + numArr[i] + ", sum is " + sum);
+        for (int num : numArr) {
+            sum += num;
+            System.out.println(numeration + ") Num is " + num + ", sum is " + sum);
             numeration++;
         }
         System.out.println("------------------------\nSum of numbers is " + sum);
